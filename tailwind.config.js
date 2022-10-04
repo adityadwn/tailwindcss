b679812dd4f8934fd8656907148b9d52581ccc3a
adityadwn <223200009@student.unaki.ac.id>
2022-10-04 16:00:26 +0700

    adding code into file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+                one: '#2A454B',
+                two: '#21373F',
+                three: '#172A32',
+                middle: '#0E1C26',
+              },
+    },
+  },
+  plugins: [],
+}
